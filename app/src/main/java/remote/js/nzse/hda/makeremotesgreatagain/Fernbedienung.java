cb4ac478aafd3d72a07c66d5b24162663d43570d
mrzeldaguy <justin.j.boehm@stud.h-da.de>
2016-11-11 13:30:47 +0100

    Added some prototype play/pause, mute/unmute, change channel functionality

--- app/src/main/java/remote/js/nzse/hda/makeremotesgreatagain/Fernbedienung.java
+++ app/src/main/java/remote/js/nzse/hda/makeremotesgreatagain/Fernbedienung.java
@@ -3,13 +3,24 @@ package remote.js.nzse.hda.makeremotesgreatagain;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Fernbedienung extends AppCompatActivity {
+
+    private Spinner sender;
+    private SeekBar vol;
+    private boolean muted=false;
+    private int volCache=50;
+    private boolean paused=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,11 @@ public class Fernbedienung extends AppCompatActivity {
         setContentView(R.layout.activity_fernbedienung);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Not really sure if we should do this or just always call findViewById if we need one of these
+        sender=(Spinner) findViewById(R.id.spinner_sender);
+        vol=(SeekBar) findViewById(R.id.bar_volume);
+        vol.setProgress(volCache);
     }
 
     @Override
@@ -39,5 +55,59 @@ public class Fernbedienung extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void playButtonPressed(View v){
+        //TODO: Implement Play/Pause Function and enable ff/rewind buttons only when paused before
+        if(paused){
+            Toast.makeText(this, "Resuming", Toast.LENGTH_LONG).show();
+            ((ImageButton)findViewById(R.id.button_play)).setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.pause));
+            paused=false;
+        }else{
+            Toast.makeText(this, "Pausing", Toast.LENGTH_LONG).show();
+            ((ImageButton)findViewById(R.id.button_play)).setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.play));
+            paused=true;
+        }
+    }
+
+    public void lastChannelButtonPressed(View v){
+        Toast.makeText(this, "Last Channel", Toast.LENGTH_LONG).show();
+        changeSender(-1);
+        //TODO: Send sender to TV
+    }
+
+    private void changeSender(int by){
+        if((sender.getSelectedItemPosition()+by)>=sender.getAdapter().getCount() || (sender.getSelectedItemPosition()+by)<0){
+            if(by>0)
+                sender.setSelection(0);
+            else
+                sender.setSelection(sender.getAdapter().getCount()-1);
+        }else{
+            sender.setSelection(sender.getSelectedItemPosition()+by);
+        }
+    }
+
+    public void nextChannelButtonPressed(View v){
+        Toast.makeText(this, "Next Channel", Toast.LENGTH_SHORT).show();
+        changeSender(1);
+        //TODO: Send sender to TV
+    }
+
+    public void muteButtonPressed(View v){
+        Toast.makeText(this, "Mute pressed", Toast.LENGTH_SHORT).show();
+        if(muted){
+            //Unmute
+            vol.setEnabled(true);
+            vol.setProgress(volCache);
+            ((ImageButton)findViewById(R.id.button_mute)).setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.mute));
+            muted=false;
+        }else{
+            //Mute
+            volCache=vol.getProgress();
+            vol.setProgress(0);
+            vol.setEnabled(false);
+            ((ImageButton)findViewById(R.id.button_mute)).setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.unmute));
+            muted=true;
+        }
     }
 }
